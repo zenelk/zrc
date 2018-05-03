5e01ec470991c21f4e5c0c2ad6dbe6ce56fa38f3
Hunter Lang <hunter.lang@workday.com>
2018-05-03 16:43:22 -0600

    All modules loading now.

--- zrc.sh
+++ zrc.sh
@@ -3,29 +3,20 @@
 # Manages module loaded / common functionality 
 # in rc files to be shared across machines/
 #############################
-export _ZRC_LOADED_MODULES=""
-export _ZRC_USE_DEFAULT_MODULE_DIRECTORY=false
-export _ZRC_DEFAULT_MODULE_DIRECTORY="$HOME/.zrcmodules"
+export _ZRC_LOADED_MODULES=()
 export _ZRC_DISABLE_MODULE_MANAGEMENT=false
 
 #############################
 # Check Config
 #############################
 
+if [ ${#ZRC_MODULES[@]} -eq 0 ]; then
+	echo "ZRC_MODULES is empty! Did you mean to do this? Define them in your '~/.zshrc' file."
+fi
+
 if [ -z $ZRC_MODULE_DIRECTORY ]; then
-	_ZRC_USE_DEFAULT_MODULE_DIRECTORY=true
-elif [ ! -e $ZRC_MODULE_DIRECTORY ]; then
-	echo "ZRC Warning: Invalid module directory '$ZRC_MODULE_DIRECTORY'! Using default at '$_ZRC_DEFAULT_MODULE_DIRECTORY'"
-	_ZRC_USE_DEFAULT_MODULE_DIRECTORY=true
-fi
-
-if [ "$_ZRC_USE_DEFAULT_MODULE_DIRECTORY" = true ]; then
-	ZRC_MODULE_DIRECTORY="$_ZRC_DEFAULT_MODULE_DIRECTORY"
-fi
-
-if [ -z $ZRC_MODULE_SRC_DIRECTORY ] || [ ! -e $ZRC_MODULE_SRC_DIRECTORY ]; then 
-	echo "ZRC Warning: No directory specified for module source. Module management is disabled."
-	_ZRC_DISABLE_MODULE_MANAGEMENT=1
+	echo "No ZRC_MODULE_DIRECTORY! Modules cannot be loaded without this path. Define it in your '~/.zshrc' file."
+	_ZRC_DISABLE_MODULE_MANAGEMENT=true
 fi
 
 #############################
@@ -44,7 +35,7 @@ zrc() {
 	case $1 in
 	mod)
 		if [ "$_ZRC_DISABLE_MODULE_MANAGEMENT" = true ]; then
-			echo "Module management is disabled because there is no module source directory specified. Define 'ZRC_MODULE_SRC_DIRECTORY' in your '~/.zshrc' and 'rez' to re-enable."
+			echo "Module management is disabled because there is no module source directory specified. Define 'ZRC_MODULE_DIRECTORY' in your '~/.zshrc' and 'rez' to re-enable."
 			return 1
 		fi
 		shift
@@ -56,16 +47,16 @@ zrc() {
 _zrc_mod() {
 	case $1 in
 	edit)
-		subl "$ZRC_MODULE_SRC_DIRECTORY"
+		subl "$ZRC_MODULE_DIRECTORY"
 		;;
 	add)
-		local sourceModulePath="$ZRC_MODULE_SRC_DIRECTORY/$2"
+		local sourceModulePath="$ZRC_MODULE_DIRECTORY/$2"
 		echo "$sourceModulePath"
 		if [ -e "$sourceModulePath" ]; then
 			local newModulePath="$ZRC_MODULE_DIRECTORY/$2"
 		elif [ -e "$sourceModulePath.zrc_module" ]; then
 			local newModulePath="$ZRC_MODULE_DIRECTORY/$2.zrc_module"
-			sourceModulePath="$ZRC_MODULE_SRC_DIRECTORY/$2.zrc_module"
+			sourceModulePath="$ZRC_MODULE_DIRECTORY/$2.zrc_module"
 		else
 			echo "Module '$2' does not exist!"
 			return 1
@@ -97,7 +88,6 @@ function is_module_loaded() {
 }
 
 function load_module() {
-	module="$1"
 	if [ ! -d "$ZRC_MODULE_DIRECTORY" ]; then
 		echo "No modules directory!"
 		return
@@ -106,24 +96,19 @@ function load_module() {
 		no_link="${module::-1}"
 		module="$(readlink "$no_link")"
 	elif [ ! -e "$module" ]; then
-		module="$ZRC_MODULE_DIRECTORY/$module.zrc_module"
+		module="$ZRC_MODULE_DIRECTORY/$module.zrc"
 		if [ ! -e "$module" ]; then
 			echo "Cannot find module $1 as itself or as path $module"
 			return
 		fi
 	fi
 	
-	export _ZRC_LOADED_MODULES="$_ZRC_LOADED_MODULES$1;"
+	_ZRC_LOADED_MODULES+=($1)
 	source "$module"
 }
 
-if [ -d "$ZRC_MODULE_DIRECTORY" ]; then
-	for module in $(ls "$ZRC_MODULE_DIRECTORY"); do
-		load_module "$ZRC_MODULE_DIRECTORY/$module"
-	done
-else 
-	echo "Creating modules directory at $ZRC_MODULE_DIRECTORY"
-	mkdir "$ZRC_MODULE_DIRECTORY"
-fi
+for module in "${ZRC_MODULES[@]}"; do
+	load_module "$module"
+done
 
 alias rez="source $0"
